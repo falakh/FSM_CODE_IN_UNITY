c01f5368c0655c226f510619586ed1c50477304b
falakh <zulfikarfahmi38@gmail.com>
2019-04-11 11:32:47 +0700

    Initial commit

---
+++ Assets/StateDead.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StateDead : FSMState
+{
+    //public bool isAdaMusuh= false;
+    public FSMSystem BaseFSMsystem;
+    public Text result;
+    
+
+    public StateDead()
+    {
+        stateID = StateID.Dead;
+    }
+
+    public override void Act( GameObject npc)
+    {
+        result.text = "Meninggal";
+    }
+
+    public override void Reason( GameObject npc)
+    {
+    
+    }
+
+
+}
---
+++ Assets/TransAman.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmpetyHealth : FSMtransition {
+
+    public void Start()
+    {
+        transition = "aman";
+    }
+
+    public override void doInTransition()
+    {
+        Debug.Log("Meninggal");
+    }
+}
